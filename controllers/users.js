d66b3a2edd4c98dac151c3fe140d4e7ff4ab230e
Danvendra <danvendra480@gmail.com>
2025-08-25 02:15:24 +0530

    removing next from signup in users.js

--- controllers/users.js
+++ controllers/users.js
@@ -9,7 +9,7 @@ module.exports.renderSignupForm=(req,res)=>{
 
 
 
-module.exports.signup = async(req,res,next)=>{
+module.exports.signup = async(req,res)=>{
     try{
         let {username, email,password} = req.body;
         const newUser = new User ({email, username,password});
